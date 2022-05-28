b8cec9013a721bda2207ad715d04d6c1e64b3682
lpm0073 <lpm0073@gmail.com>
2022-05-28 17:15:18 -0500

    backup should be mfe

--- scripts/upload-to-aws-ecr.sh
+++ scripts/upload-to-aws-ecr.sh
@@ -4,6 +4,6 @@
 # Push and tag the newly created Docker image.
 #
 #---------------------------------------------------------
-tutor images push backup
+tutor images push mfe
 docker tag ${AWS_ECR_REGISTRY}/${AWS_ECR_REPOSITORY}:${REPOSITORY_TAG} ${AWS_ECR_REGISTRY}/${AWS_ECR_REPOSITORY}:latest
 docker push ${AWS_ECR_REGISTRY}/${AWS_ECR_REPOSITORY}:latest
